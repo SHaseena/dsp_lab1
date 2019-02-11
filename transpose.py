776a53405ede18c428244ffb1cc1966b043f1cfc
SHaseena <47116673+SHaseena@users.noreply.github.com>
2019-02-11 23:50:15 +0530

    Add files via upload

---
+++ transpose.py
@@ -0,0 +1,14 @@
+m=int(input("enter the rows:"))
+n=int(input("enter the columns:"))
+matrix=[[0 for j in range(0,n)] for i in range(0,m)]
+matrix1=[[0 for j in range(0,n)] for i in range(0,m)]
+print("enter the matrix elements:")
+for i in range(0,m):
+	for j in range(0,n):
+		matrix[i][j]=int(input("enter elements:")
+print(matrix)
+print("transpose of matrix1")
+for i in range(len(matrix))
+for j in range(len(matrix[0])):
+matrix1[j][i]=matrix[i][j]
+print(matrix1)
