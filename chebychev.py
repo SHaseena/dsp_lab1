45c45da24f8a761bfd771bb47445208970cb439e
SHaseena <47116673+SHaseena@users.noreply.github.com>
2019-04-23 17:07:32 +0530

    Add files via upload

---
+++ chebychev.py
@@ -0,0 +1,7 @@
+import numpy as np
+from pylab import *
+import matplotlib.pyplot as plt
+ws=float(input("enter wp value:"))
+wp=float(input("enter ws value:"))
+dp=float(input("enter pass band gain:"))
+ds=float(input("enter stop band gain:"))
